e9482a4c05a5979ffef3714f69860c6ebfd5d074
Bart Judge <bartjudge@Barts-MacBook-Pro.local>
2020-04-29 17:22:38 +0100

    weather condtions now affect landings and takeoffs

--- jasmine-standalone-3/spec/AirportSpec.js
+++ jasmine-standalone-3/spec/AirportSpec.js
@@ -5,22 +5,32 @@ describe('Airport', function() {
     airport = new Airport(2);
     plane = new Plane(); //change to a jasmine spy
   });
+  describe('during bad weather', function() {
+    it('will not allow a plane to take off in stormy weather', function() {
+      expect(airport.takeoff(plane, 'STORMY')).toEqual("Unable to take off due to bad weather");
+    });
+    it('will not allow a plane to land in stormy weather', function() {
+      expect(airport.land(plane, 'STORMY')).toEqual("Unable to land due to bad weather");
+    });
+  });
 
-  it('lands a plane', function() {
-    expect(airport.land(plane)).toEqual("plane landed");
-  });
-  it('lets a plane take off', function() {
-    expect(airport.takeoff(plane)).toEqual("plane has left the airport");
-  });
+  describe('during good weather', function() {
+    it('lands a plane', function() {
+      expect(airport.land(plane, 'NOT-STORMY')).toEqual("plane landed");
+    });
+    it('lets a plane take off', function() {
+      expect(airport.takeoff(plane, 'NOT-STORMY')).toEqual("plane has left the airport");
+    });
 
-  it('will not allow more than two planes to land', function() {
-    plane2 = new Plane();
-    plane3 = new Plane();
-    airport.land(plane)
-    airport.land(plane2)
-    expect(airport.land(plane3)).toEqual("Unable to Land. Airport Full");
-  });
-  it('allows a plane limit at creation.', function() {
-    expect(airport.limit).toEqual(2);
+    it('will not allow more than two planes to land', function() {
+      plane2 = new Plane();
+      plane3 = new Plane();
+      airport.land(plane)
+      airport.land(plane2)
+      expect(airport.land(plane3)).toEqual("Unable to Land. Airport Full");
+    });
+    it('allows a plane limit at creation.', function() {
+      expect(airport.limit).toEqual(2);
+    });
   });
 });
--- jasmine-standalone-3/src/Airport.js
+++ jasmine-standalone-3/src/Airport.js
@@ -1,20 +1,36 @@
 
 var Airport = function(limit) {
   this.hangar = [];
-  this.limit = limit;
+  this.limit = limit || 2;
 };
 
-Airport.prototype.land = function(plane) {
-  if(this.hangar.length + 1 > this.limit) {
-  return "Unable to Land. Airport Full";
-} if(this.hangar.length  < this.limit) {
-    this.hangar.push(plane);
-    return "plane landed";
-}
+Airport.prototype.land = function(plane, weather) {
+  if(weather === 'STORMY') {
+    return "Unable to land due to bad weather";
+  };
+    if(this.hangar.length + 1 > this.limit) {
+    return "Unable to Land. Airport Full";
+  } if(this.hangar.length  < this.limit) {
+      this.hangar.push(plane);
+      return "plane landed";
+  }
 
 };
 
-Airport.prototype.takeoff = function(plane) {
+Airport.prototype.takeoff = function(plane, weather) {
+  if (weather === 'STORMY') {
+    return "Unable to take off due to bad weather";
+  }
   this.hangar.pop(plane);
   return "plane has left the airport";
 };
+
+
+/* code for random weather function. should be its own class,
+so I would put in its only file when it's needed.
+
+const weatherOptions = ['STORMY', 'NOT-STORMY'];
+
+function randomWeather(weatherOptions) {
+  return weatherOptions[Math.floor(Math.random() * weatherOptions.length)];
+} */
