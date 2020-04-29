0fbe0d798efd60dcbe5c52c700d54738072c1347
Bart Judge <bartjudge@Barts-MacBook-Pro.local>
2020-04-29 16:05:39 +0100

    third user story

--- jasmine-standalone-3/spec/AirportSpec.js
+++ jasmine-standalone-3/spec/AirportSpec.js
@@ -3,13 +3,21 @@ describe('Airport', function() {
   var plane;
   beforeEach(function() {
     airport = new Airport();
-    plane = new Plane();
+    plane = new Plane(); //change to a jasmine spy
   });
-  
+
   it('lands a plane', function() {
     expect(airport.land(plane)).toEqual("plane landed");
   });
   it('lets a plane take off', function() {
     expect(airport.takeoff(plane)).toEqual("plane has left the airport");
+  });
+
+  it('will not allow more than two planes to land', function() {
+    plane2 = new Plane();
+    plane3 = new Plane();
+    airport.land(plane)
+    airport.land(plane2)
+    expect(airport.land(plane3)).toEqual("Unable to Land. Airport Full");
   });
 });
--- jasmine-standalone-3/src/Airport.js
+++ jasmine-standalone-3/src/Airport.js
@@ -1,9 +1,18 @@
-var Airport = function() {
 
+var Airport = function() {
+  this.hangar = [];
 };
 
+const limit = 2;
+
 Airport.prototype.land = function(plane) {
-  return "plane landed";
+  if(this.hangar.length + 1 > limit) {
+  return "Unable to Land. Airport Full";
+} if(this.hangar.length  <=limit) {
+    this.hangar.push(plane);
+    return "plane landed";
+}
+
 };
 
 Airport.prototype.takeoff = function(plane) {
